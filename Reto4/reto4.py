321b6149b8f545abe641b3b591713042f7b70881
David Trigo <david.trigo@gmail.com>
2022-02-15 00:15:26 +0100

    Reto4 solved

--- Reto4/reto4.py
+++ Reto4/reto4.py
@@ -3,14 +3,35 @@
 from tkinter import SE
 
 
-class Serie():
+class Entregable():
+
+    def __init__(self, entregado):
+        self.__entregado = entregado
+
+    def entregar(self):
+        self.__entregado = True
+
+    def devolver(self):
+        self.__entregado = False
+    
+    def isEntregado(self):
+        return self.__entregado
+
+    def compareTo(self, a):
+        pass
+
+    def __str__(self) -> str:
+        pass
+
+
+class Serie(Entregable):
 
     def __init__(self, titulo = '', numero_temporadas = 3, entregado = False, genero = '', creador = ''):
         self.__titulo = titulo
         self.__numero_temporadas = numero_temporadas
-        self.__entregado = entregado
         self.__genero = genero
         self.__creador = creador
+        Entregable.__init__(self, entregado)
 
     @property
     def titulo(self):
@@ -45,17 +66,25 @@ class Serie():
         self.__creador = creador
 
     def __str__(self) -> str:
-        return f"Titulo: {self.__titulo}\nNumero de temporadas: {self.__numero_temporadas}\nEntregado: {self.__entregado}\nGenero: {self.__genero}\nCreador: {self.__creador}"
+        return f"Titulo: {self.__titulo}\nNumero de temporadas: {self.__numero_temporadas}\nEntregado: {self.isEntregado()}\nGenero: {self.__genero}\nCreador: {self.__creador}"
+
+    def compareTo(self, a):
+        diferencia_temporadas = self.__numero_temporadas - a.__numero_temporadas
+        if diferencia_temporadas >= 0:
+            print(f"La otra serie tiene {diferencia_temporadas} temporadas menos ")
+        else:
+            print(f"La otra serie tiene {diferencia_temporadas*-1} temporadas mas")
 
 
-class Videojuego():
+
+class Videojuego(Entregable):
     
     def __init__(self, titulo = "", horas_estimadas = 10, entregado = False, genero = "", company = "") -> None:
         self.__titulo = titulo
         self.__horas_estimadas = horas_estimadas
-        self.__entregado = entregado
         self.__genero = genero
         self.__company = company
+        Entregable.__init__(self, entregado)
 
     @property
     def titulo(self):
@@ -91,83 +120,102 @@ class Videojuego():
         self.__company = company
 
     def __str__(self) -> str:
-        return f"Titulo: {self.__titulo}\nNumero de horas estimadas de juego: {self.__horas_estimadas}\nEntregado: {self.__entregado}\nGenero: {self.__genero}\nCompañia: {self.__company}"
-
-
-class Entregable(Serie, Videojuego):
-
-    def __init__(self, titulo='', numero_temporadas=3, entregado=False, genero='', creador='', horas_estimadas = 10, company = ''):
-        Serie.__init__(self,titulo, numero_temporadas, entregado, genero, creador)
-        Videojuego.__init__(self, titulo, horas_estimadas, entregado, genero, company)
-
-    def entregar(self):
-        self.__entregado = True
-
-    def devolver(self):
-        self.__entregado = False
-    
-    def isEntregado(self):
-        return self.__entregado
+        return f"Titulo: {self.__titulo}\nNumero de horas estimadas de juego: {self.__horas_estimadas}\nEntregado: {self.isEntregado()}\nGenero: {self.__genero}\nCompañia: {self.__company}"
 
     def compareTo(self, a):
-        # if isinstance(a,Videojuego):
-        if self.company != '' and a.company != '':
-            diferencia_horas = self.horas_estimadas - a.horas_estimadas
-            if diferencia_horas >= 0:
-                print(f"El otro juego tiene {diferencia_horas} horas menos de juego")
-            else:
-                print(f"El otro juego tiene {diferencia_horas*-1} horas mas de juego")
-        elif self.creador != '' and a.creador != '':
-            diferencia_temporadas = self.numero_temporadas - a.numero_temporadas
-            if diferencia_temporadas >= 0:
-                print(f"La otra serie tiene {diferencia_temporadas} temporadas menos ")
-            else:
-                print(f"La otra serie tiene {diferencia_temporadas*-1} temporadas mas")
+        diferencia_horas = self.__horas_estimadas - a.__horas_estimadas
+        if diferencia_horas >= 0:
+            print(f"El otro juego tiene {diferencia_horas} horas menos de juego")
         else:
-            print("Estas comparando series y peliculas, no es una comparacion valida")
-
-    def __str__(self) -> str:
-        if self.company != '':
-            return Videojuego.__str__(self)
-        return Serie.__str__(self)
+            print(f"El otro juego tiene {diferencia_horas*-1} horas mas de juego")
 
 
-serie = Serie("Salvados", 7, True, "Misterio", "XX")
-# print(serie.titulo)
-# print(serie)
 
-juego1 = Videojuego("Carmagedon", 500, False, "Coches", "XX")
+# serie = Serie("Salvados", 7, True, "Misterio", "XX")
+# # print(serie.titulo)
+# # print(serie)
 
-# juego1.compareTo(juego2)
-# juego1.compareTo(juego3)
+# juego1 = Videojuego("Carmagedon", 500, False, "Coches", "XX")
 
-entregable1 = Entregable(titulo="Salvados", numero_temporadas=7, entregado=True, genero="Misterio", creador="XX")
-entregable2 = Entregable(titulo="Fringe", numero_temporadas=8, entregado=True, genero="Misterio", creador="XX")
-entregable3 = Entregable(titulo="Carmagedon", horas_estimadas=500, entregado=False, genero="Coches", company="XX")
-entregable4 = Entregable(titulo="Carmagedon2", horas_estimadas=400, entregado=False, genero="Coches", company="XX")
+# # juego1.compareTo(juego2)
+# # juego1.compareTo(juego3)
+
+serie1 = Serie(titulo="Salvados", numero_temporadas=7, entregado=True, genero="Misterio", creador="XX")
+serie2 = Serie(titulo="Fringe", numero_temporadas=8, entregado=True, genero="Misterio", creador="XX")
+videjuego1 = Videojuego(titulo="Carmagedon", horas_estimadas=500, entregado=False, genero="Coches", company="XX")
+videjuego2 = Videojuego(titulo="Carmagedon2", horas_estimadas=400, entregado=False, genero="Coches", company="XX")
 
 
-print(entregable1)
-print("---------------")
-print(entregable2)
-print("---------------")
-entregable1.compareTo(entregable2)
-print("---------------")
-entregable3.compareTo(entregable4)
-print("---------------")
-entregable3.compareTo(entregable2)
+# print(serie1)
+# print("---------------")
+# print(serie2)
+# print("---------------")
+# serie1.compareTo(serie2)
+# print("---------------")
+# videjuego1.compareTo(videjuego2)
 
+# print(serie1)
+# print(serie1.entregar())
+# print(serie1.isEntregado())
+# print(serie1.devolver())
+# print(serie1.isEntregado())
+
+# print(videjuego1)
+# print(videjuego1.entregar())
+# print(videjuego1.isEntregado())
+# print(videjuego1.devolver())
+# print(videjuego1.isEntregado())
 
 
 if __name__ == "__main__":
 
-    peliculas = []
+    series = []
     videojuegos = []
 
+    # Data inizialization
     for i in range(5):
-        peliculas.append(Serie(f"Serie {i}", 7+i, True, "Misterio", "XX"))
+        series.append(Serie(f"Serie {i}", 7+i, False, "Misterio", "XX"))
         videojuegos.append(Videojuego(f"Juego {i}", 500+i, False, "Coches", "XX"))
     
+    # Deliver some series and games
+    for i in range(len(series)):
+        if i%2 == 0:
+            series[i].entregar()
+
+    for i in range(len(videojuegos)):
+        if i%2 == 0:
+            videojuegos[i].entregar()
     
-    
+    # Count delivered series and games
+    delivered_series = 0
+    delivered_games = 0
+
+    for i in range(len(series)):
+        if series[i].isEntregado():
+            delivered_series += 1
+            
+
+    for i in range(len(videojuegos)):
+        if videojuegos[i].isEntregado():
+            delivered_games += 1
+
+    print(f"Se han entregado {delivered_series} series y {delivered_games} videojuegos")
+
+    # Search longest game and serie
+    longest_serie = series[0]
+    longest_game = videojuegos[0]
+
+    for i in range(len(series)):
+        if series[i].numero_temporadas > longest_serie.numero_temporadas:
+            longest_serie = series[i]
+            
+
+    for i in range(len(videojuegos)):
+        if videojuegos[i].horas_estimadas > longest_game.horas_estimadas:
+            longest_game = videojuegos[i]
+
+    print(f"El videojuego mas largo es {longest_game}")
+    print(f"La serie con mas capitulos es {longest_serie}")
+
+
     
