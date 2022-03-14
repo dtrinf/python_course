097a05b7c27874c5f8d3085708e0614c85013192
David Trigo <david.trigo@gmail.com>
2022-03-15 00:28:18 +0100

    user model

--- FinalProject/final_project/web_lfg/models.py
+++ FinalProject/final_project/web_lfg/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class player(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    date_of_birth = models.DateField(null=True)
+    steam_user = models.CharField(max_length = 100)
+    twitter_user = models.CharField(max_length = 100)
+    discord_user = models.CharField(max_length = 100)
+
+    def __str__(self):
+        return self.user.username
+
+
+
